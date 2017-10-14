e8d9bd5643fc4960dd47186df0a5aae4479bc274
aguxez <dizmiiguel@gmail.com>
2017-10-14 18:51:33 -0400

    WIP to return correct values correctly

--- lib/palsound/retriever/videos.ex
+++ lib/palsound/retriever/videos.ex
@@ -10,6 +10,8 @@ defmodule Palsound.Retriever.Videos do
   # @playlist_id "PLbggoxi0-M8ytOrH6_-pS6ynp8bAWgHUK"
   # Palsound.Retriever.Videos.run("PLbggoxi0-M8ytOrH6_-pS6ynp8bAWgHUK")
 
+  # TODO: Return error values appropiately.
+
   # Agent
   def start_link do
     Agent.start_link(fn -> [] end, name: :videos)
@@ -28,29 +30,39 @@ defmodule Palsound.Retriever.Videos do
   end
 
   # Server and API
-  def run(playlist) do
-    songs =
-      playlist
-      |> get_list()
-      |> List.flatten()
+  def run(playlist_id, amount \\ nil) do
+    playlist = get_list(playlist_id)
+    songs = fn ->
+      if amount do
+        playlist
+        |> List.flatten()
+        |> Enum.take(amount)
+      else
+        playlist_id
+        |> get_list()
+        |> List.flatten()
+      end
+    end
 
     unless File.exists?("songs"), do: File.mkdir("songs")
 
     Checker.start_link(:songs)
+    Checker.queue(:songs, songs.())
 
-    IO.puts("Started checkers")
-
-    Checker.queue(:songs, songs)
-
-    IO.puts("Queued songs")
+    queued = "Queued songs"
+    IO.puts(queued)
+    queued
   end
 
   def get_list(playlist_id, opts \\ []) do
     defaults = [maxResults: 50]
 
-    {:ok, playlist, meta} =
-      Playlist.get_items(playlist_id, Keyword.merge(defaults, opts))
+    playlist_id
+    |> Playlist.get_items(Keyword.merge(defaults, opts))
+    |> process_playlist(defaults, playlist_id)
+  end
 
+  defp process_playlist({:ok, playlist, meta}, defaults, playlist_id) do
     next_page = meta["nextPageToken"]
 
     playlist
@@ -66,6 +78,8 @@ defmodule Palsound.Retriever.Videos do
       state
     end
   end
+
+  defp process_playlist({:error, _}, _, _), do: []
 
   def queue_and_download(songs, songs_path) do
     Enum.each(songs, fn curr_song ->
